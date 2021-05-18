7a0b7055fa5ec6e91d2f5b67265131059a2d97df
Rizky92 <myquinn489@gmail.com>
2021-05-18 10:33:01 +0800

    up

--- routes/api.php
+++ routes/api.php
@@ -23,86 +23,20 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::group(['prefix' => 'admin'], function () {
     Route::resource('products', App\Http\Controllers\API\ProductAPIController::class);
-});
-
-
-Route::group(['prefix' => 'admin'], function () {
     Route::resource('product_stocks', App\Http\Controllers\API\ProductStockAPIController::class);
-});
-
-
-Route::group(['prefix' => 'admin'], function () {
     Route::resource('carts', App\Http\Controllers\API\CartAPIController::class);
-});
-
-
-Route::group(['prefix' => 'admin'], function () {
     Route::resource('cart_details', App\Http\Controllers\API\CartDetailAPIController::class);
-});
-
-
-Route::group(['prefix' => 'admin'], function () {
     Route::resource('orders', App\Http\Controllers\API\OrderAPIController::class);
-});
-
-
-Route::group(['prefix' => 'admin'], function () {
     Route::resource('order_details', App\Http\Controllers\API\OrderDetailAPIController::class);
-});
-
-
-Route::group(['prefix' => 'admin'], function () {
     Route::resource('transactions', App\Http\Controllers\API\TransactionAPIController::class);
-});
-
-
-Route::group(['prefix' => 'admin'], function () {
     Route::resource('transaction_details', App\Http\Controllers\API\TransactionDetailAPIController::class);
-});
-
-
-
-
-Route::group(['prefix' => 'admin'], function () {
     Route::resource('discounts', App\Http\Controllers\API\DiscountAPIController::class);
-});
-
-
-Route::group(['prefix' => 'admin'], function () {
     Route::resource('discount_details', App\Http\Controllers\API\DiscountDetailAPIController::class);
-});
-
-
-Route::group(['prefix' => 'admin'], function () {
     Route::resource('events', App\Http\Controllers\API\EventAPIController::class);
-});
-
-
-Route::group(['prefix' => 'admin'], function () {
     Route::resource('files', App\Http\Controllers\API\FileAPIController::class);
-});
-
-
-Route::group(['prefix' => 'admin'], function () {
     Route::resource('file_thumbs', App\Http\Controllers\API\FileThumbAPIController::class);
-});
-
-
-Route::group(['prefix' => 'admin'], function () {
     Route::resource('post_categories', App\Http\Controllers\API\PostCategoryAPIController::class);
-});
-
-
-Route::group(['prefix' => 'admin'], function () {
     Route::resource('posts', App\Http\Controllers\API\PostAPIController::class);
-});
-
-
-Route::group(['prefix' => 'admin'], function () {
     Route::resource('shipments', App\Http\Controllers\API\ShipmentAPIController::class);
-});
-
-
-Route::group(['prefix' => 'admin'], function () {
     Route::resource('categories', App\Http\Controllers\API\CategoryAPIController::class);
 });
--- routes/web.php
+++ routes/web.php
@@ -61,146 +61,51 @@ Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', App\Http\Controllers\UserController::class);
 
     Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-
+        Route::resource('activities', App\Http\Controllers\ActivityController::class)
+            ->names('activities');
+        Route::resource('reports', App\Http\Controllers\ReportController::class)
+            ->names('reports');
+        Route::resource('statuses', App\Http\Controllers\StatusController::class)
+            ->names('statuses');
+        Route::resource('partners', App\Http\Controllers\PartnerController::class)
+            ->names('partners');
+        Route::resource('spendings', App\Http\Controllers\SpendingController::class)
+            ->names('spendings');
+        Route::resource('spending_details', App\Http\Controllers\SpendingDetailController::class)
+            ->names('spendingDetails');
+        Route::resource('products', App\Http\Controllers\ProductController::class)
+            ->names('products');
+        Route::resource('product_stocks', App\Http\Controllers\ProductStockController::class)
+            ->names('productStocks');
+        Route::resource('carts', App\Http\Controllers\CartController::class)
+            ->names('carts');
+        Route::resource('cart_details', App\Http\Controllers\CartDetailController::class)
+            ->names('cartDetails');
+        Route::resource('orders', App\Http\Controllers\OrderController::class)
+            ->names('orders');
+        Route::resource('order_details', App\Http\Controllers\OrderDetailController::class)
+            ->names('orderDetails');
+        Route::resource('transactions', App\Http\Controllers\TransactionController::class)
+            ->names('transactions');
+        Route::resource('transaction_details', App\Http\Controllers\TransactionDetailController::class)
+            ->names('transactionDetails');
+        Route::resource('discounts', App\Http\Controllers\DiscountController::class)
+            ->names('discounts');
+        Route::resource('discount_details', App\Http\Controllers\DiscountDetailController::class)
+            ->names('discountDetails');
+        Route::resource('events', App\Http\Controllers\EventController::class)
+            ->names('events');
+        Route::resource('files', App\Http\Controllers\FileController::class)
+            ->names('files');
+        Route::resource('file_thumbs', App\Http\Controllers\FileThumbController::class)
+            ->names('fileThumbs');
+        Route::resource('post_categories', App\Http\Controllers\PostCategoryController::class)
+            ->names('postCategories');
+        Route::resource('posts', App\Http\Controllers\PostController::class)
+            ->names('posts');
+        Route::resource('shipments', App\Http\Controllers\ShipmentController::class)
+            ->names('shipments');
+        Route::resource('categories', App\Http\Controllers\CategoryController::class)
+            ->names('categories');
     });
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('activities', App\Http\Controllers\ActivityController::class)
-        ->names('activities');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('reports', App\Http\Controllers\ReportController::class)
-        ->names('reports');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('statuses', App\Http\Controllers\StatusController::class)
-        ->names('statuses');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('partners', App\Http\Controllers\PartnerController::class)
-        ->names('partners');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('spendings', App\Http\Controllers\SpendingController::class)
-        ->names('spendings');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('spending_details', App\Http\Controllers\SpendingDetailController::class)
-        ->names('spendingDetails');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('products', App\Http\Controllers\ProductController::class)
-        ->names('products');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('product_stocks', App\Http\Controllers\ProductStockController::class)
-        ->names('productStocks');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('carts', App\Http\Controllers\CartController::class)
-        ->names('carts');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('cart_details', App\Http\Controllers\CartDetailController::class)
-        ->names('cartDetails');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('orders', App\Http\Controllers\OrderController::class)
-        ->names('orders');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('order_details', App\Http\Controllers\OrderDetailController::class)
-        ->names('orderDetails');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('transactions', App\Http\Controllers\TransactionController::class)
-        ->names('transactions');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('transaction_details', App\Http\Controllers\TransactionDetailController::class)
-        ->names('transactionDetails');
-});
-
-
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('discounts', App\Http\Controllers\DiscountController::class)
-        ->names('discounts');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('discount_details', App\Http\Controllers\DiscountDetailController::class)
-        ->names('discountDetails');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('events', App\Http\Controllers\EventController::class)
-        ->names('events');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('files', App\Http\Controllers\FileController::class)
-        ->names('files');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('file_thumbs', App\Http\Controllers\FileThumbController::class)
-        ->names('fileThumbs');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('post_categories', App\Http\Controllers\PostCategoryController::class)
-        ->names('postCategories');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('posts', App\Http\Controllers\PostController::class)
-        ->names('posts');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('shipments', App\Http\Controllers\ShipmentController::class)
-        ->names('shipments');
-});
-
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('categories', App\Http\Controllers\CategoryController::class)
-        ->names('categories');
 });
