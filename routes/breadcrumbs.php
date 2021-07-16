202589ec0920b8c5bd68c8b39c060009e131b55d
Rizky92 <myquinn489@gmail.com>
2021-07-17 04:50:23 +0800

    update

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -5,5 +5,5 @@
 use Diglactic\Breadcrumbs\Breadcrumbs;
 
 // This import is also not required, and you could replace `BreadcrumbTrail $trail`
-//  with `$trail`. This is nice for IDE type checking and completion.
+// with `$trail`. This is nice for IDE type checking and completion.
 use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
